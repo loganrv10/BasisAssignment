f68b2170f0db809bab581477ab02cbc1b187436f
loganrv10 <80710806+loganrv10@users.noreply.github.com>
2022-01-19 23:30:45 +0530

    adapter created

---
+++ app/src/main/java/com/subham/Basisswipeapp/ui/CardAdapter.kt
@@ -0,0 +1,45 @@
+package com.subham.Basisswipeapp.ui
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Adapter
+import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.RecyclerView
+import com.subham.Basisswipeapp.DataItem
+import com.subham.Basisswipeapp.R
+import kotlinx.android.synthetic.main.new_layout.view.*
+
+class CardAdapter(private val list:ArrayList<DataItem>): RecyclerView.Adapter<CardAdapter.CardViewHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CardViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.new_layout,parent,false)
+        return CardViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: CardViewHolder, position: Int) {
+        holder.setData(list[position])
+
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+
+
+
+    class CardViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        fun setData(dataItem: DataItem){
+            itemView.apply {
+                TvPageNum.text = dataItem.id
+                TvId.text = dataItem.id
+                TvText.text = dataItem.text
+            }
+
+        }
+    }
+
+}
